6120480a17dd3f16c801cb2cf1ff28ff30ba0455
YoBiya <nanashigure@extra.ocn.ne.jp>
2023-10-01 02:33:47 +0900

    MAUIのプロジェクトを追加

---
+++ HierarchyBox/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HierarchyBox
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
