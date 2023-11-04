eedcfc78b43c7b05b43bd5003ede4bfdda917236
YoBiya <nanashigure@extra.ocn.ne.jp>
2023-11-04 17:50:39 +0900

    フォルダの選択機能を追加

--- HierarchyBox/Views/FileExplorer/SelectFolderPage.xaml.cs
+++ HierarchyBox/Views/FileExplorer/SelectFolderPage.xaml.cs
@@ -1,3 +1,5 @@
+using CommunityToolkit.Maui.Storage;
+
 namespace HierarchyBox.Views.FileExplorer;
 
 public partial class SelectFolderPage : ContentPage
@@ -5,5 +7,18 @@ public partial class SelectFolderPage : ContentPage
 	public SelectFolderPage()
 	{
 		InitializeComponent();
+
+        Loaded += async (_, _) =>
+        {
+            var source = new CancellationTokenSource();
+            var result = await FolderPicker.Default.PickAsync("~", source.Token);
+            if (result.IsSuccessful)
+            {
+            }
+            else
+            {
+                await Navigation.PopAsync();
+            }
+        };
 	}
 }
