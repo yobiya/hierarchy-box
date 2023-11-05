c8acfbc502f404c1b0a0119858fc3026f6122318
YoBiya <nanashigure@extra.ocn.ne.jp>
2023-11-05 20:40:25 +0900

    ディレクトリの階層表示に対応

--- HierarchyBox/ViewModels/FileExplorer/DirectoryViewModel.cs
+++ HierarchyBox/ViewModels/FileExplorer/DirectoryViewModel.cs
@@ -5,12 +5,14 @@ namespace HierarchyBox.ViewModels.FileExplorer
     public class DirectoryViewModel
     {
         public string Name { get; }
-        public IEnumerable CellListNames { get; }
+        public IEnumerable NameList { get; }
+        public IEnumerable BoxList { get; }
 
         public DirectoryViewModel(string directoryPath)
         {
             Name = Path.GetFileName(directoryPath);
-            CellListNames = Directory.EnumerateFiles(directoryPath).Select(path => Path.GetFileName(path)).ToList();
+            NameList = Directory.EnumerateFiles(directoryPath).Select(path => Path.GetFileName(path));
+            BoxList = Directory.EnumerateDirectories(directoryPath).Select(path => new DirectoryViewModel(path));
         }
     }
 }
