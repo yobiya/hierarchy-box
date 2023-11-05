a5af2c0032df66f7903ad1c724a70f1d6ce6dc1c
YoBiya <nanashigure@extra.ocn.ne.jp>
2023-11-05 18:09:11 +0900

    フォルダ内のファイル名を表示する機能を追加

--- HierarchyBox/ViewModels/FileExplorer/DirectoryViewModel.cs
+++ HierarchyBox/ViewModels/FileExplorer/DirectoryViewModel.cs
@@ -1,12 +1,16 @@
-﻿namespace HierarchyBox.ViewModels.FileExplorer
+﻿using System.Collections;
+
+namespace HierarchyBox.ViewModels.FileExplorer
 {
     public class DirectoryViewModel
     {
         public string Name { get; }
+        public IEnumerable CellListNames { get; }
 
         public DirectoryViewModel(string directoryPath)
         {
             Name = Path.GetFileName(directoryPath);
+            CellListNames = Directory.EnumerateFiles(directoryPath).Select(path => Path.GetFileName(path)).ToList();
         }
     }
 }
