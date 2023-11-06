41001ba753b2722422b320f533a40dbf708026bf
YoBiya <nanashigure@extra.ocn.ne.jp>
2023-11-07 00:00:52 +0900

    ディレクトリ名のボタンの呼び出しメソッドを追加

--- HierarchyBox/ViewModels/FileExplorer/DirectoryViewModel.cs
+++ HierarchyBox/ViewModels/FileExplorer/DirectoryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Reactive.Bindings;
+using System.Collections;
 
 namespace HierarchyBox.ViewModels.FileExplorer
 {
@@ -7,12 +8,18 @@ namespace HierarchyBox.ViewModels.FileExplorer
         public string Name { get; }
         public IEnumerable NameList { get; }
         public IEnumerable BoxList { get; }
+        public ReactiveCommand OnClickedDirectoryName { get; }
 
         public DirectoryViewModel(string directoryPath)
         {
             Name = Path.GetFileName(directoryPath);
             NameList = Directory.EnumerateFiles(directoryPath).Select(path => Path.GetFileName(path));
             BoxList = Directory.EnumerateDirectories(directoryPath).Select(path => new DirectoryViewModel(path));
+            OnClickedDirectoryName = new ReactiveCommand().WithSubscribe(ToggleDirectoryOpenClose);
+        }
+
+        private void ToggleDirectoryOpenClose()
+        {
         }
     }
 }
