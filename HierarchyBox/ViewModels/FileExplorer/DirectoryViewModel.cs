f83c6dc6d8a8ddd1a7a00d8f7d961db89a7cfeaf
YoBiya <nanashigure@extra.ocn.ne.jp>
2023-11-08 00:39:46 +0900

    バインドするコレクションをReactiveCollectionに変更

--- HierarchyBox/ViewModels/FileExplorer/DirectoryViewModel.cs
+++ HierarchyBox/ViewModels/FileExplorer/DirectoryViewModel.cs
@@ -6,15 +6,21 @@ namespace HierarchyBox.ViewModels.FileExplorer
     public class DirectoryViewModel
     {
         public string Name { get; }
-        public IEnumerable NameList { get; }
+        public ReactiveCollection<string> NameList { get; private set; } = new();
         public IEnumerable BoxList { get; }
         public ReactiveCommand OnClickedDirectoryName { get; }
 
         public DirectoryViewModel(string directoryPath)
         {
             Name = Path.GetFileName(directoryPath);
-            NameList = Directory.EnumerateFiles(directoryPath).Select(path => Path.GetFileName(path));
+
+            foreach (var name in Directory.EnumerateFiles(directoryPath).Select(Path.GetFileName))
+            {
+                NameList.Add(name);
+            }
+
             BoxList = Directory.EnumerateDirectories(directoryPath).Select(path => new DirectoryViewModel(path));
+
             OnClickedDirectoryName = new ReactiveCommand().WithSubscribe(ToggleDirectoryOpenClose);
         }
 
