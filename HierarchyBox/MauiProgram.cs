a77dc922d23a24d92c066a3a856abf5a2b996d62
YoBiya <nanashigure@extra.ocn.ne.jp>
2023-11-10 02:27:13 +0900

    アプリケーションの配布設定を追加

--- HierarchyBox/MauiProgram.cs
+++ HierarchyBox/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace HierarchyBox
 {
@@ -14,9 +15,10 @@ namespace HierarchyBox
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+            builder.UseMauiCommunityToolkit();
 
 #if DEBUG
-		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
