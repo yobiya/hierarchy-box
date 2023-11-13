e05e16c4c274adf0ca25dbe00229322217a76445
YoBiya <nanashigure@extra.ocn.ne.jp>
2023-11-14 01:15:33 +0900

    ファイルを開く機能を追加

--- HierarchyBox/ViewModels/FileExplorer/FileViewModel.cs
+++ HierarchyBox/ViewModels/FileExplorer/FileViewModel.cs
@@ -5,12 +5,16 @@ namespace HierarchyBox.ViewModels.FileExplorer
 {
     public partial class FileViewModel : ObservableObject
     {
+        private readonly string _fullPath;
+
         public string Name { get; }
 
         public ICommand OnRequestContextMenuItem { get; }
 
         public FileViewModel(string directoryPath, string fileName)
         {
+            _fullPath = Path.Combine(directoryPath, fileName);
+
             Name = fileName;
 
             OnRequestContextMenuItem = new Command(CallContextMenu);
@@ -26,6 +30,7 @@ namespace HierarchyBox.ViewModels.FileExplorer
 
             if (commandName == "open")
             {
+                Launcher.Default.OpenAsync(new OpenFileRequest("Open", new ReadOnlyFile(_fullPath)));
             }
         }
     }
