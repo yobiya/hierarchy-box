d17d9cb92c9134dc6fe89f00e9f027fc4c3c46f9
YoBiya <nanashigure@extra.ocn.ne.jp>
2023-11-16 00:21:27 +0900

    ディレクトリの一覧をVerticalStackLayoutで表示するように変更

--- HierarchyBox/Views/FileExplorer/DirectoryBoxView.xaml.cs
+++ HierarchyBox/Views/FileExplorer/DirectoryBoxView.xaml.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace HierarchyBox.Views.FileExplorer;
 
 public partial class DirectoryBoxView : ContentView
@@ -5,5 +7,21 @@ public partial class DirectoryBoxView : ContentView
 	public DirectoryBoxView()
 	{
 		InitializeComponent();
+
+        BoxStackLayout.BindingContextChanged += DirectoryBoxView_BindingContextChanged;
 	}
+
+    private void DirectoryBoxView_BindingContextChanged(object sender, EventArgs e)
+    {
+        BoxStackLayout.Clear();
+
+        var childContexts = BoxStackLayout.BindingContext as IEnumerable;
+        
+        foreach (var childContext in childContexts)
+        {
+            var childBoxView = new DirectoryBoxView();
+            childBoxView.BindingContext = childContext;
+            BoxStackLayout.Add(childBoxView);
+        }
+    }
 }
