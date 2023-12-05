12e1fd931726e0df78e3f412cc4867ef269e3902
YoBiya <nanashigure@extra.ocn.ne.jp>
2023-12-05 17:33:05 +0900

    設定ファイルの配置されているディレクトリを開く機能を追加

--- HierarchyBox/MainPage.xaml.cs
+++ HierarchyBox/MainPage.xaml.cs
@@ -1,15 +1,22 @@
-﻿namespace HierarchyBox
-{
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+﻿using HierarchyBox.Views.FileExplorer;
+using System.Diagnostics;
 
-        private async void OnFileExplorerButtonClicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new Views.FileExplorer.SelectFolderPage());
-        }
+namespace HierarchyBox;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnFileExplorerButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Views.FileExplorer.SelectFolderPage());
+    }
+
+    private void OnSettingFileDirectoryOpenButtonClicked(object sender, EventArgs e)
+    {
+        Process.Start("explorer.exe", ViewDefinitions.GetApplicationLocalDirectoryPath());
     }
 }
