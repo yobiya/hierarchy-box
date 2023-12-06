419acb3eabdaf44e1dacaebb46752993eba53a53
YoBiya <nanashigure@extra.ocn.ne.jp>
2023-12-06 16:28:07 +0900

    ファイル名の定義を追加

--- HierarchyBox/Models/FileExplorer/CommandDefinitions.cs
+++ HierarchyBox/Models/FileExplorer/CommandDefinitions.cs
@@ -3,5 +3,6 @@ namespace HierarchyBox.Models.FileExplorer
     public static class CommandDefinitions
     {
         public const string DirectoryPathReplaceTag = "{DIRECTORY_PATH}";
+        public const string FileNameReplaceTag = "{FILE_NAME}";
     }
 }
